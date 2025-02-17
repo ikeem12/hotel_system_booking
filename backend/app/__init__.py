06b82fade6925a6acbde42a07aaf38e8d2e88dce
johan sebastian <baloncestonba222@gmail.com>
2025-02-17 10:03:24 +0100

    feat: agregar blueprint de reservation_service_v1

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from config import Config
 from .core.extensions import db, limiter
+from .services.reservation_services.v1 import bp_rsv_svc_v1
 
 def create_app():
     app = Flask(__name__)
@@ -10,5 +11,10 @@ def create_app():
     db.init_app(app)
     limiter.storage_uri = app.config["STORAGE_URI"]
     limiter.init_app(app)
+
+    app.register_blueprint(
+        bp_rsv_svc_v1,
+        url_prefix='/api/v1'
+    )
 
     return app
---
+++ backend/app/services/reservation_services/v1/__init__.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+bp_rsv_svc_v1 = Blueprint('reservation_services_v1', __name__)
