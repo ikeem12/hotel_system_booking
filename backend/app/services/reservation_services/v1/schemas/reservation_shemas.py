cdb31ddc744ffeabbf5db8540ec824fab22ca6a6
johan sebastian <baloncestonba222@gmail.com>
2025-02-17 09:46:25 +0100

    feat: agregar validacion de campos a ReservationSchema con Pydantic

---
+++ backend/app/services/reservation_services/v1/schemas/reservation_shemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional,Dict, Literal
+
+class ReservationShemas(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    phone: Optional[str] = None
+    check_in: str
+    check_out: str
+    rooms: Dict[Literal['Individual','Doble','Familiar','Suite'], int]
